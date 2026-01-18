baf29ec24ecf0a47434285426da049347db23ffd
nishanshaju22 <nishan.shaju@icloud.com>
2026-01-18 22:29:34 +0800

    individual fighter stats added

--- data_processor/fight_stats.py
+++ data_processor/fight_stats.py
@@ -17,3 +17,50 @@ def fighterDataframe(df: pd.DataFrame):
     df['target'] = (df['Winner'] == 'Fighter 1').astype(int)
     convert_to_csv(df)
     return df
+
+
+def fighterStats(df: pd.DataFrame):
+    
+    cols = ["Name", "KD", "STR", "TD", "SUB", "Winner"]
+    
+    fighter_1 = df[[
+        "Fighter 1",
+        "Fighter_1_KD",
+        "Fighter_1_STR",
+        "Fighter_1_TD",
+        "Fighter_1_SUB",
+        "Winner"
+    ]].copy()
+    
+    
+    fighter_1.columns = cols
+    fighter_1["WINS"] = (fighter_1["Name"] == fighter_1["Winner"]).astype(int)
+    fighter_1["Fights"] = 1
+    
+    fighter_2 = df[[
+        "Fighter 2",
+        "Fighter_2_KD",
+        "Fighter_2_STR",
+        "Fighter_2_TD",
+        "Fighter_2_SUB",
+        "Winner"
+    ]].copy()
+    
+    fighter_2.columns = cols
+    fighter_2["WINS"] = (fighter_2["Name"] == fighter_2["Winner"]).astype(int)
+    fighter_2["Fights"] = 1
+    
+    
+    fighters = pd.concat([fighter_1, fighter_2], ignore_index=False)
+    
+    
+    fighter_stats = fighters.groupby("Name").agg(
+        total_KD=("KD", "sum"),
+        total_STR=("STR", "sum"),
+        total_TD=("TD", "sum"),
+        total_SUB=("SUB", "sum"),
+        total_wins=("WINS", "sum"),
+        total_fights=("Fights", "sum")
+    )
+    
+    return fighter_stats
