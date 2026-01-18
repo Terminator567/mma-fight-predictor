0ca1b12ad7655bf0fde46ea5871827609f25bb52
Sheelav Prakash <z5485139@ad.unsw.edu.au>
2026-01-18 12:31:47 +0800

    first commit

---
+++ notebooks/.ipynb_checkpoints/explore-checkpoint.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv('../data/ufc export 20260117 094436.csv')
+data.head()
---
+++ notebooks/explore.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = pd.read_csv('/Users/sheelavprakash/Desktop/mma-fight-predictor/data/ufc export 2026-01-17 09-44-36.csv')
+print("First 5 fights:")
+print(data.head())
+
+print("\n")
+print(f"Total fights: {len(data)}")
