302a60c6e619c5d0a3d2db807f4acb4cd15080d9
nishanshaju22 <nishan.shaju@icloud.com>
2026-01-18 16:44:38 +0800

    data retrival and sending to csv chnaged

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from data_processor.fight_stats import fightStats
 from data.get_data import get_dataframe
 from data_processor.data_types_fixes import check_and_process_data_type
 
-df = get_dataframe()
+df = get_dataframe("original.csv")
 # check_and_process_data_type(df)
-# fightStats(df)
+df1 = fightStats(df)
+
+print(df1.head())
 
