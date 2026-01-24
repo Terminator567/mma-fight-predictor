4b1aa5fd986e3bcf1e613b26927891e8e191bfd9
nishanshaju22 <nishan.shaju@icloud.com>
2026-01-24 15:40:21 +0800

    final proccesing for model creatiion

--- data_processor/data_categorising.py
+++ data_processor/data_categorising.py
@@ -4,7 +4,7 @@ import pandas as pd
 def categories_columns(df: DataFrame):
     df = df.copy()
     
-    col_to_codes = ["Fighter 1", "Fighter 2" ,"Weight_Class", "Method", "Winner"]
+    col_to_codes = ["Fighter", "Opp"]
     
     for col in col_to_codes:
         df[f"{col}_code"] = df[col].astype("category").cat.codes
--- data_processor/fight_stats.py
+++ data_processor/fight_stats.py
@@ -1,79 +1,49 @@
 import pandas as pd
 
-def fightStats(df: pd.DataFrame):
-    df['KD_diff'] = df['Fighter_1_KD'] - df['Fighter_2_KD']
-    df['STR_diff'] = df['Fighter_1_STR'] - df['Fighter_2_STR']
-    df['TD_diff'] = df['Fighter_1_TD'] - df['Fighter_2_TD']
-    df['SUB_diff'] = df['Fighter_1_SUB'] - df['Fighter_2_SUB']
-    
-    return df
+def finalProcessingForFighter(df: pd.DataFrame) -> pd.DataFrame:
+    cols = ["Fighter", "Opp", "Date", "KD", "STR", "TD", "SUB", "Weight_Class", "Winner"]
 
-
-def fighterDataframe(df: pd.DataFrame):
-    df['target'] = (df['Winner'] == df['Fighter 1']).astype(int)
-    return df
-
-
-import pandas as pd
-
-def fighterStats(processed_df: pd.DataFrame) -> pd.DataFrame:
-    original_df = pd.read_csv('../data/original.csv')
-
-    cols = ["Name", "KD", "STR", "TD", "SUB", "Winner"]
-
-    fighter_1 = processed_df[[
+    fighter_1 = df[[
         "Fighter 1",
-        "Fighter_1_KD",
-        "Fighter_1_STR",
-        "Fighter_1_TD",
-        "Fighter_1_SUB",
+        "Fighter 2",
+        "Date",
+        "Fighter_1_total_KD",
+        "Fighter_1_total_STR",
+        "Fighter_1_total_TD",
+        "Fighter_1_total_SUB",
+        "Weight_Class",
         "Winner"
     ]].copy()
     fighter_1.columns = cols
-    fighter_1["WINS"] = (fighter_1["Name"] == fighter_1["Winner"]).astype(int)
+    fighter_1["Target"] = (fighter_1["Fighter"] == fighter_1["Winner"]).astype(int)
     fighter_1["Fights"] = 1
 
-    fighter_2 = processed_df[[
+    fighter_2 = df[[
         "Fighter 2",
-        "Fighter_2_KD",
-        "Fighter_2_STR",
-        "Fighter_2_TD",
-        "Fighter_2_SUB",
+        "Fighter 1",
+        "Date",
+        "Fighter_1_total_KD",
+        "Fighter_1_total_STR",
+        "Fighter_1_total_TD",
+        "Fighter_1_total_SUB",
+        "Weight_Class",
         "Winner"
     ]].copy()
     fighter_2.columns = cols
-    fighter_2["WINS"] = (fighter_2["Name"] == fighter_2["Winner"]).astype(int)
+    fighter_2["Target"] = (fighter_2["Fighter"] == fighter_2["Winner"]).astype(int)
     fighter_2["Fights"] = 1
 
     fighters = pd.concat([fighter_1, fighter_2], ignore_index=True)
-
-    fighter_stats = fighters.groupby("Name", as_index=False).agg(
-        avg_KD=("KD", "mean"),
-        avg_STR=("STR", "mean"),
-        avg_TD=("TD", "mean"),
-        avg_SUB=("SUB", "mean"),
-        win_rate=("WINS", "mean")
+    
+    fighter_stats = (
+        fighters.loc[:, ["Fighter", "Date", "Opp", "Weight_Class", "KD", "STR", "TD", "SUB", "Target"]]
+        .drop_duplicates(subset=["Fighter", "Date"])
+        .rename(columns={
+            "KD": "Total_KD",
+            "STR": "Total_STR",
+            "TD": "Total_TD",
+            "SUB": "Total_SUB"
+        })
     )
-
-    original_df = original_df.merge(
-        fighter_stats.add_prefix("F1_"),
-        left_on="Fighter 1",
-        right_on="F1_Name",
-        how="left"
-    ).drop(columns=["F1_Name"])
-
-    original_df = original_df.merge(
-        fighter_stats.add_prefix("F2_"),
-        left_on="Fighter 2",
-        right_on="F2_Name",
-        how="left"
-    ).drop(columns=["F2_Name"])
-
-    return original_df
-
-
-df = pd.read_csv('../data/original.csv')
-df = fightStats(df)
-df = fighterDataframe(df)
-#df = fighterStats(df)
-df.to_csv('../data/processed.csv', index=False)
+    
+    return fighter_stats
