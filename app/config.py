6c7aea648c4a27b1f9941d212e21087a4c1e7e07
nishanshaju22 <nishan.shaju@icloud.com>
2026-01-29 15:15:13 +0800

    cleaning up repo

--- app/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    ORIGINAL_CSV = "ufc_with_career_stats.csv"
-    PROCESSED_CSV = "processed.csv"
--- notebooks/.ipynb_checkpoints/explore-checkpoint.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-data = pd.read_csv('../data/ufc export 20260117 094436.csv')
-data.head()
