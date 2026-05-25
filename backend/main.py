ff4547560714ac665ca67c3a809421f843f069d1
Sheelav Prakash <z5485139@ad.unsw.edu.au>
2026-05-26 09:54:48 +1000

    fixed routes in backend

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,20 @@ from pathlib import Path
 
 app = create_app()
 
-PROJECT_ROOT = Path(__file__).parent.absolute()
-FRONTEND_DIR = str(PROJECT_ROOT / 'frontend')
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = PROJECT_ROOT / "frontend"
 
 @app.route('/frontend/<path:filename>')
 def serve_static(filename):
     return send_from_directory(FRONTEND_DIR, filename)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return send_from_directory(FRONTEND_DIR, 'index.html')
+    return send_from_directory(FRONTEND_DIR, "index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(FRONTEND_DIR, path)
 
 if __name__ == '__main__':
     app.run(
